a3ed131bf0dd534b647e92d12f7d726dc662ee02
davina1995grayson-max <davina1995grayson@gmail.com>
2026-06-05 16:14:34 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@500;600;700&display=swap" rel="stylesheet" />
+      </head>
+
       <body>{children}</body>
     </html>
   );
