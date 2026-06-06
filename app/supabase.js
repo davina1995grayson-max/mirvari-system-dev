ddd64e5cb236b2e9245c46999fa590fa4c4f0041
davina1995grayson-max <davina1995grayson@gmail.com>
2026-06-06 20:10:45 +0400

    Create supabase.js

---
+++ app/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://gayfaxpdbccwtoojelek.supabase.com";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheWZheHBkYmNjd3Rvb2plbGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1OTU5NDEsImV4cCI6MjA5NjE3MTk0MX0.YSIPUPCgzx-lZguTnPH1gXTx846xsd06rGHKeNccm_c";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
